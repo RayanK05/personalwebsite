ff76746fa876eda4a2b57890db87face9185a2de
RayanK05 <rayan.khalil@ontariotechu.net>
2025-05-09 02:01:29 -0400

    Typewriter Effect

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
-$(function(){
+var messageArray = ["Good Day, I'm Rayan"];
+var speed = 100;
+var textPosition = 0;
 
-    window.sr = ScrollReveal();
+const typewriter = () => {
+    document.querySelector("#line1").innerHTML =
+        messageArray[0].substring(0, textPosition);
 
-    if ($(window).width() < 768) {
-
-        if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-            $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-        }
-
-        sr.reveal('.js--fadeInRight', {
-            origin: 'right',
-            distance: '300px',
-            easing: 'ease-in-out',
-            duration: 800,
-        });
-
-    } else {
-
-        sr.reveal('.js--fadeInLeft', {
-            origin: 'left',
-            distance: '300px',
-            easing: 'ease-in-out',
-            duration: 800,
-        });
-
-        sr.reveal('.js--fadeInRight', {
-            origin: 'right',
-            distance: '300px',
-            easing: 'ease-in-out',
-            duration: 800,
-        });
-
+    if (textPosition++ !== messageArray[0].length) {
+        setTimeout(typewriter, speed);
     }
+};
 
-    sr.reveal('.js--fadeInLeft', {
-        origin: 'left',
-        distance: '300px',
-        easing: 'ease-in-out',
-        duration: 800,
-    });
+window.addEventListener("load", typewriter);
 
-    sr.reveal('.js--fadeInRight', {
-        origin: 'right',
-        distance: '300px',
-        easing: 'ease-in-out',
-        duration: 800,
-    });
-
-
-});
+// $(function(){
+//
+//     window.sr = ScrollReveal();
+//
+//     if ($(window).width() < 768) {
+//
+//         if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+//             $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+//         }
+//
+//         sr.reveal('.js--fadeInRight', {
+//             origin: 'right',
+//             distance: '300px',
+//             easing: 'ease-in-out',
+//             duration: 800,
+//         });
+//
+//     } else {
+//
+//         sr.reveal('.js--fadeInLeft', {
+//             origin: 'left',
+//             distance: '300px',
+//             easing: 'ease-in-out',
+//             duration: 800,
+//         });
+//
+//         sr.reveal('.js--fadeInRight', {
+//             origin: 'right',
+//             distance: '300px',
+//             easing: 'ease-in-out',
+//             duration: 800,
+//         });
+//
+//     }
+//
+//     sr.reveal('.js--fadeInLeft', {
+//         origin: 'left',
+//         distance: '300px',
+//         easing: 'ease-in-out',
+//         duration: 800,
+//     });
+//
+//     sr.reveal('.js--fadeInRight', {
+//         origin: 'right',
+//         distance: '300px',
+//         easing: 'ease-in-out',
+//         duration: 800,
+//     });
+//
+//
+// });
